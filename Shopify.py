137d77aa9852f116622fbbc36e71a434eff2d766
Muhammad Waseem <43111670+iamwasii@users.noreply.github.com>
2019-08-16 16:58:43 +0500

    Update Shopify.py

--- Shopify.py
+++ Shopify.py
@@ -5,7 +5,8 @@ import json
 
 class Shopify():
     def __init__(self):
-        self.connection_string = "https://587de113912bfa3dd33e5d05950b4ace:ca3bfda6ffd75cb48c05b24f3cd25905@inshout.myshopify.com/admin/api/2019-04/"
+        self.connection_string = \
+            "https://587de113912bfa3dd33e5d05950b4ace:ca3bfda6ffd75cb48c05b24f3cd25905@inshout.myshopify.com/admin/api/2019-07/"
 
     def get_product(self, product_id):
         try:
@@ -86,6 +87,23 @@ class Shopify():
     def edit_variant(self, product_to_be_updated, product):
         variants = []
         for variant in product['variants']:
+            # image_id = variant['image_id']
+            # for image in product['images']:
+            #     if image['id'] == image_id:
+            #         img = {
+            #             'width': image['width'],
+            #             'height': image['height'],
+            #             'src': image['src']
+            #         }
+            #         image_res = requests.post(
+            #             "{connection_string}/products/{product_id}/images.json".format(
+            #                 connection_string=self.connection_string,
+            #                 product_id=product_to_be_updated['id']),
+            #             data=json.dumps({"image": img}),
+            #             headers={'content-type': 'Application/json'}
+            #         )
+            #         if image_res == 200 or 201:
+            #             print(product_to_be_updated['title'])
             vari = {
                 'product_id': variant['product_id'],
                 'title': variant['title'],
@@ -112,40 +130,72 @@ class Shopify():
             }
             options.append(opt)
         data = {
-            "id": product_to_be_updated['id'],
-            "handle": product['handle'],
+            "id": product_to_be_updated.id,
             "variants": variants,
             "options": options
         }
         try:
             res = requests.put(
                 "{connection_string}/products/{product_id}.json".format(connection_string=self.connection_string,
-                                                                        product_id=product_to_be_updated['id']),
+                                                                        product_id=product_to_be_updated.id),
                 data=json.dumps({"product": data}),
                 headers={'content-type': 'Application/json'}
             )
             if res.status_code == 200 or 201:
-                print(product_to_be_updated['title'])
+                print(product_to_be_updated.title)
 
         except Exception as e:
             logging.exception(e)
             logging.info("Variant not found")
 
-    def edit_product(self, product):
-        new_title = product['title'].replace("-", " ")
+    def update_variant(self, product_to_be_updated, product):
+        i = 0
+        for variant in product['variants']:
+            to_be_updated = product_to_be_updated['variants'][i]
+            invent_qt = 0
+            if variant['available']:
+                if to_be_updated['inventory_quantity'] > 0:
+                    pass
+                else:
+                    invent_qt += 1
+            else:
+                if to_be_updated['inventory_quantity'] > 0:
+                    invent_qt -= 1
+            vari = {
+                'price': str(float(variant['price']) * 1.7),
+                'compare_at_price': None,
+                'taxable': variant['taxable'],
+                'grams': variant['grams'],
+                'inventory_quantity': invent_qt,
+                'old_inventory_quantity': 0,
+                'requires_shipping': variant['requires_shipping'],
+            }
+            try:
+                res = requests.put(
+                    "{connection_string}/variants/{variant_id}.json".format(connection_string=self.connection_string,
+                                                                            variant_id=to_be_updated['id']),
+                    data=json.dumps({"variant": vari}),
+                    headers={'content-type': 'Application/json'}
+                )
+            except Exception as e:
+                logging.exception(e)
+                logging.info("Variant not found")
+            i = i + 1
+
+    def edit_product_title(self, product, title):
         data = {
-            "id": product['id'],
-            "title": new_title
+            "id": product.id,
+            "title": title
         }
         try:
             res = requests.put(
                 "{connection_string}/products/{product_id}.json".format(connection_string=self.connection_string,
-                                                                        product_id=product['id']),
+                                                                        product_id=product.id),
                 data=json.dumps({"product": data}),
                 headers={'content-type': 'Application/json'}
             )
-            if res.text:
-                print(product['title'])
+            if res.status_code == 200 or 201:
+                print(product.title+"             "+product.handle)
 
         except Exception as e:
             logging.exception(e)
@@ -220,40 +270,150 @@ class Shopify():
                 logging.exception(e)
                 logging.info("Product not found")
 
+    def create_order_webhook(self):
+        data = {
+            'topic': 'orders/create',
+            'address': 'https://order-details.free.beeceptor.com',
+            'format': 'json'
+        }
+        try:
+            res = requests.post(
+                "{connection_string}/webhooks.json".format(connection_string=self.connection_string),
+                data=json.dumps({"webhook": data}),
+                headers={'content-type': 'Application/json'}
+            )
+            if res.status_code == 201:
+                print("Webhook created")
+            else:
+                print("Webhook not created")
+        except Exception as e:
+            logging.exception(e)
+            logging.info("Something went wrong")
+
+    def get_all_webhooks(self):
+        try:
+            res = requests.get(
+                "{connection_string}/webhooks.json".format(connection_string=self.connection_string),
+                headers={'content-type': 'Application/json'}
+            )
+            if res.status_code == 200:
+                json_data = res.json()
+                print(json_data)
+        except Exception as e:
+            logging.exception(e)
+            logging.info("Something went wrong")
+
+    def delete_product(self, product):
+        try:
+            res = requests.delete(
+                "{connection_string}/products/{product_id}.json".format(connection_string=self.connection_string,
+                                                                        product_id=product.id))
+            if res.status_code == 200:
+                print(product.title)
+
+        except Exception as e:
+            logging.exception(e)
+            logging.info("Product not found")
+
+    def update_variant_titles(self, product):
+        print(product.title)
+        for variant in product.variants:
+            vari = {
+                'id': variant.id,
+                'option1': variant.option1,
+                'option2': variant.option2,
+                'option3': variant.option3
+            }
+            try:
+                res = requests.put(
+                    "{connection_string}/variants/{variant_id}.json".format(
+                        connection_string=self.connection_string,
+                        variant_id=variant.id),
+                    data=json.dumps({"variant": vari}),
+                    headers={'content-type': 'Application/json'}
+                )
+            except Exception as e:
+                logging.exception(e)
+                logging.info("Variant not found")
+
+    def update_tags(self, product):
+        data = {
+            'tags': product.tags
+        }
+        try:
+            res = requests.put(
+                "{connection_string}/products/{product_id}.json".format(connection_string=self.connection_string,
+                                                                        product_id=product.id),
+                data=json.dumps({"product": data}),
+                headers={'content-type': 'Application/json'}
+            )
+            print(product.title)
+        except Exception as e:
+            logging.exception(e)
+            logging.info("Not Updated")
+
+    def edit_product_vendor_and_title(self, product):
+        data = {
+            "id": product.id,
+            "title": product.title
+        }
+        try:
+            res = requests.put(
+                "{connection_string}/products/{product_id}.json".format(connection_string=self.connection_string,
+                                                                        product_id=product.id),
+                data=json.dumps({"product": data}),
+                headers={'content-type': 'Application/json'}
+            )
+            if res.status_code == 200 or 201:
+                print(product.title)
+
+        except Exception as e:
+            logging.exception(e)
+            logging.info("Product not found")
+
+    def create_image_of_variants(self, source_product, store_product):
+        if len(source_product['variants']) == len(store_product.variants):
+            for image in source_product['images']:
+                if image['variant_ids']:
+                    source_url = image['src']
+                    store_ids = []
+                    for _id in image['variant_ids']:
+                        for source, store in zip(source_product['variants'], store_product.variants):
+                            if _id == source['id'] and store.image_id is None:
+                                store_ids.append(store.id)
+                    if store_ids:
+                        data = {
+                            'variant_ids': store_ids,
+                            'src': source_url
+                        }
+                        res = requests.post(
+                            "{connection_string}/products/{product_id}/images.json".format(
+                                connection_string=self.connection_string,
+                                product_id=store_product.id),
+                            data=json.dumps({"image": data}),
+                            headers={'content-type': 'Application/json'}
+                        )
+                        if res == 200 or 201:
+                            print(store_product.title)
+
+    def update_handle(self, product, handle):
+        data = {
+            'handle': handle
+        }
+        try:
+            res = requests.put(
+                "{connection_string}/products/{product_id}.json".format(connection_string=self.connection_string,
+                                                                        product_id=product['id']),
+                data=json.dumps({"product": data}),
+                headers={'content-type': 'Application/json'}
+            )
+            print(product['title'])
+        except Exception as e:
+            logging.exception(e)
+            logging.info("Not Updated")
+
 
 if __name__ == '__main__':
     shop = Shopify()
     all_products = shop.get_all_store_products()
     print(all_products)
-    # product = shop.get_product("3659372068944")
-    # updated_variant = {
-    #     'id': 28695680514206,
-    #     'product_id': 3659372068944,
-    #     'title': 'S / Cream',
-    #     'price': '799.00',
-    #     'sku': '8946 L-946.1018-00040',
-    #     'position': 2,
-    #     'inventory_policy': 'deny',
-    #     'compare_at_price': None,
-    #     'fulfillment_service': 'manual',
-    #     'inventory_management': 'shopify',
-    #     'option1': 'S',
-    #     'option2': 'Cream',
-    #     'option3': None,
-    #     'created_at': '2019-07-09T21:07:00+05:00',
-    #     'updated_at': '2019-07-09T21:07:00+05:00',
-    #     'taxable': False,
-    #     'barcode': '',
-    #     'grams': 275,
-    #     'image_id': None,
-    #     'weight': 275.0,
-    #     'weight_unit': 'g',
-    #     'inventory_item_id': 29642944413776,
-    #     'inventory_quantity': 1,
-    #     'old_inventory_quantity': 0,
-    #     'requires_shipping': True,
-    #     'admin_graphql_api_id': 'gid://shopify/ProductVariant/28695680516176'
-    # }
-    # print(shop.edit_variant("28695680514206", updated_variant))
-    # print(shop.get_product_all_variants("3659372068944"))
-    # print(shop.get_product_all_images("3659372068944"))
